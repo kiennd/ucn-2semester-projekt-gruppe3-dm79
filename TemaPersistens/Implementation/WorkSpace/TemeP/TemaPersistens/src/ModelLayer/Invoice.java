011e5f0690d428328a0fb8411fff0ff3a4ceef09
jakob.kaspersen <jakob.kaspersen@gmail.com>
2012-10-23 18:52:03 +0000

    Updated

--- TemaPersistens/Implementation/WorkSpace/TemeP/TemaPersistens/src/ModelLayer/Invoice.java
+++ TemaPersistens/Implementation/WorkSpace/TemeP/TemaPersistens/src/ModelLayer/Invoice.java
@@ -2,4 +2,42 @@ package ModelLayer;
 
 public class Invoice {
 
+	private int invoiceNo;
+	private String paymentDate;
+	private double totalAmount;
+	
+	private Invoice (int invoiceNo, String paymentDate, double totalAmount)
+	{
+	this.invoiceNo = invoiceNo;
+	this.paymentDate = paymentDate;
+	this.totalAmount = totalAmount;
+	}
+
+	public int getInvoiceNo() {
+		return invoiceNo;
+	}
+
+	public void setInvoiceNo(int invoiceNo) {
+		this.invoiceNo = invoiceNo;
+	}
+
+	public String getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(String paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	
+	
+	
+	
 }
