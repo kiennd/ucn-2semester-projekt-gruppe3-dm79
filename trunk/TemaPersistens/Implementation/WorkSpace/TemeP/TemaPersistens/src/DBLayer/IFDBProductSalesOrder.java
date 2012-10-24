c2a2da7db2d29e357db70df1f672626cb3157e64
faith.ogini.nielsen@gmail.com <faith.ogini.nielsen@gmail.com@7d988f38-3376-e13e-69bc-206b7a1aac3e>
2012-10-24 10:17:12 +0000

    added classes for DB layer

---
+++ trunk/TemaPersistens/Implementation/WorkSpace/TemeP/TemaPersistens/src/DBLayer/IFDBProductSalesOrder.java
@@ -0,0 +1,14 @@
+package DBLayer;
+
+import ModelLayer.*;
+import java.util.ArrayList;
+
+/**
+ * 
+ * Interface for DBProductSalesOrder.
+ *
+ */
+
+public interface IFDBProductSalesOrder {
+
+}
---
+++ trunk/TemaPersistens/Implementation/WorkSpace/TemeP/TemaPersistens/src/DBLayer/IFDBSupplier.java
@@ -0,0 +1,14 @@
+package DBLayer;
+
+import ModelLayer.*;
+import java.util.ArrayList;
+
+/**
+ * 
+ * Interface for DBSupplier.
+ *
+ */
+
+public interface IFDBSupplier {
+
+}
